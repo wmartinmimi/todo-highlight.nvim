bc46a44719d86d1bd916ce79fc8787d9714ce8ea
wmartinmimi <wmartinmimipub@gmail.com>
2026-01-23 14:57:57 +0000

    add treesitter support

--- lua/todo-highlight.lua
+++ lua/todo-highlight.lua
@@ -9,17 +9,14 @@ local defaults = {
 }
 
 local api = vim.api
+local ts = vim.treesitter
 local ns = api.nvim_create_namespace("todo-highlight")
 
 function M.say_hello()
     print("hello from neovim")
 end
 
-local function highlight_visible_todos(bufnr, winid)
-  -- Clear only our decorations
-  api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-
-  -- Visible window range (1-based)
+local function raw_buffer_highlight(bufnr, winid)
   local first = vim.fn.line("w0", winid) - 1
   local last  = vim.fn.line("w$", winid)
 
@@ -32,8 +29,7 @@ local function highlight_visible_todos(bufnr, winid)
 
       local matcher = tag .. "%:"
 
-
-      local s, e = string.find(line, matcher, 1)
+      local s, e = line:find(matcher, 1)
 
       if s then
           -- highlight todo
@@ -43,6 +39,65 @@ local function highlight_visible_todos(bufnr, winid)
         })
       end
     end
+  end
+end
+
+local function ts_highlight(bufnr, winid, lang)
+
+  local ok, parser = pcall(ts.get_parser, bufnr, lang)
+  if not ok then
+    return
+  end
+
+  local tree = parser:parse()[1]
+  local root = tree:root()
+
+  local query = ts.query.parse(lang, "(comment) @comment")
+
+  for _, node in query:iter_captures(root, bufnr, 0, -1) do
+    
+    local text = ts.get_node_text(node, bufnr)
+
+    for i, line in ipairs(vim.split(text, "\r?\n")) do
+      
+      local start_row, start_col = node:start()
+      local col_offset = 0
+
+      if i == 1 then
+        col_offset = start_col
+      end
+
+      for tag, hl in pairs(M.opts.tags) do
+
+        local matcher = tag .. "%:"
+
+        local s, e = line:find(matcher, 1)
+
+        if s then
+          print(start_row + i - 1)
+          -- highlight todo
+          api.nvim_buf_set_extmark(bufnr, ns, start_row + i - 1, col_offset + s - 1, {
+            end_col = col_offset + e,
+            hl_group = hl,
+          })
+        end
+      end
+    end
+  end
+end
+
+local function highlight_visible_todos(bufnr, winid)
+  -- Clear only our decorations
+  api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+
+  local lang = ts.language.get_lang(
+    api.nvim_buf_get_option(bufnr, "filetype")
+  )
+
+  if lang then
+    ts_highlight(bufnr, winid, lang)
+  else
+    raw_buffer_highlight(bufnr, winid)
   end
 end
 
